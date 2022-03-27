4b28c5291df619cf21d79eefc81d85fa140a28fa
paolabasso <91506356+paolabasso@users.noreply.github.com>
2022-03-26 21:54:38 -0300

    Bot comunicação com back-end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import Message from './components/message';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  const [content, setContent] = useState('');
+
+  const [session, setSession] = useState('');
+
+  const addMessage = (message, origin) => {
+    const msgs = [...messages];
+    msgs.push({ content: message, origin: origin });
+    setMessages(msgs);
+    console.log(msgs);
+  };
+
+  const handleChangeContent = e => {
+    setContent(e.target.value);
+  };
+
+  const sendMessage = async message => {
+    const url = 'http://localhost:3000';
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        sessionid: session,
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({ message: message })
+    });
+
+    const { step, sessionId } = await res.json();
+    const msgs = [];
+    msgs.push({
+      content: step.question,
+      origin: 'bot'
+    });
+    step.options.forEach(option => {
+      msgs.push({
+        content: option.message,
+        origin: 'bot'
+      });
+    });
+    setMessages([...messages, ...msgs]);
+    setSession(sessionId);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    addMessage(content, 'user');
+    sendMessage(content);
+    setContent('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="messages">
+        {messages.map((msg, index) => (
+          <Message key={index} message={msg} />
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={content} onChange={handleChangeContent} />
+        <button type="submit">Enviar</button>
+      </form>
     </div>
   );
 }
---
+++ src/components/message/index.jsx
@@ -0,0 +1,9 @@
+const Message = ({message}) => {
+  return ( 
+    <div>
+      {message.content}
+    </div>
+   );
+}
+ 
+export default Message;
